0da5efdc5bca6c463f8a87854d8ac8936c65ab4b
Ayanwales <Ayanwalesulaymon@gmail.com>
2022-06-22 12:23:17 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const myName = prompt('Enter your name');
+const myHeight = prompt('Enter your height');
+const myCountry = prompt('Enter your country');
+
+alert('My name is ' + myName +'\n'+ 'I am of height '+ myHeight + 'metres'+ '\n' + 'My country name is '+ myCountry); 
